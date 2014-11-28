82bf9a1aa9c5a718de7f839a76313763baa288a3
robbisg <robbenson18@gmail.com>
2014-11-28 01:01:05 +0100

    BF: WashUHeader readable from fileobj

--- mvpa_itab/washu.py
+++ mvpa_itab/washu.py
@@ -1,7 +1,10 @@
 from nibabel.analyze import AnalyzeImage, AnalyzeHeader
+from nibabel.spm99analyze import Spm99AnalyzeHeader
 from nibabel.spatialimages import HeaderTypeError
-from nibabel.volumeutils import make_dt_codes
 import numpy as np
+from nibabel.volumeutils import (pretty_mapping, endian_codes, native_code,
+                          swapped_code, make_dt_codes)
+from nibabel import spm99analyze
 
 # Sub-parts of standard 4dfp/ifh header from
 # Washington School of Medicine
@@ -51,7 +54,9 @@ mri_info_dtd = [
     ('mri_sequence_file_name', 'S30'),
     ('mri_sequence_description', 'S10'),
     ]
-
+analyze_hdr = spm99analyze.header_key_dtd + \
+                spm99analyze.data_history_dtd + \
+                spm99analyze.image_dimension_dtd
 # Full header numpy dtype combined across sub-fields
 header_dtype = np.dtype(acquisition_key_dtd + \
                         data_key_dtd + \
@@ -59,7 +64,8 @@ header_dtype = np.dtype(acquisition_key_dtd + \
                         image_size_dtd + \
                         image_dimension_dtd + \
                         mri_info_dtd + \
-                        data_dtype_dtd)
+                        data_dtype_dtd + \
+                        analyze_hdr)
 
 _dtdefs = ( # code, conversion function, equivalent dtype, aliases
     (0, 'none', np.void),
@@ -67,7 +73,8 @@ _dtdefs = ( # code, conversion function, equivalent dtype, aliases
     (2, 'uint8', np.uint8),
     (4, 'int16', np.int16),
     (8, 'int32', np.int32),
-    (16, 'float32', np.float32),
+    #(16, 'float32', np.float32),
+    (16, 'float32', np.dtype('>f4')),
     (32, 'complex64', np.complex64), # numpy complex format?
     (64, 'float64', np.float64),
     (128, 'RGB', np.dtype([('R','u1'),
@@ -79,7 +86,7 @@ _dtdefs = ( # code, conversion function, equivalent dtype, aliases
 data_type_codes = make_dt_codes(_dtdefs)
 
 
-class WashUHeader(AnalyzeHeader):
+class WashUHeader(Spm99AnalyzeHeader):
         
     # Copies of module-level definitions
     template_dtype = header_dtype
@@ -94,21 +101,29 @@ class WashUHeader(AnalyzeHeader):
     has_data_intercept = False
     
     
-    def __init__(self):
+    def __init__(self, header_dict):
         
-        self._header_dict = None
+        Spm99AnalyzeHeader.__init__(self)
         self._general = None
         self._data_type = None
-        AnalyzeHeader.__init__(self)
+        self._header_dict = header_dict
+        self.set_header_info()
         
+    
+    @classmethod
+    def default_structarr(klass, endianness=None):
+        return super(WashUHeader, klass).default_structarr(endianness=endianness)
+        
+    
     @classmethod
     def from_fileobj(klass, fileobj, endianness=None, check=True):
         
-        hdr = klass()
-        hdr._header_dict = hdr.file_to_dict(fileobj)
+        header_dict = klass.file_to_dict(fileobj)
+
+        return klass(header_dict)
         
-    
-    def file_to_dict(self, fileobj, separator=':= '):
+    @classmethod
+    def file_to_dict(klass, fileobj, separator=':= '):
         
         b = fileobj.readline()
         if b.find('INTERFILE') == -1:
@@ -122,38 +137,77 @@ class WashUHeader(AnalyzeHeader):
                 key_, value_ = b.split(separator)
             except ValueError:
                 break                
-            key_ = self.clean_key(key_)
-            value_ = self.parse_key_value(key_, value_, self.template_dtype)
+            key_ = clean_key(key_)
+            value_ = parse_key_value(key_, value_, header_dtype)
             header_dict[str(key_)] = value_
         
+        fileobj.close()
+
         return header_dict
-    
-            
-    def clean_key(self, key_):
-        
-        return key_.replace(' ','_').split('__')[0]
-    
-    
-    def parse_key_value(self, key_, value_, dtype_):
-        
-        data_shape = []
-        if key_ in dict(image_size_dtd).keys():
-            data_shape.append(np.array(value_, dtype=dtype_[key_]))
-        elif key_ in dict(data_dtype_dtd).keys():
-            if self._data_type == None:
-                continue
+
+    @classmethod
+    def guessed_endian(klass, hdr):
+        if hdr._header_dict['imagedata_byte_order'] == 'bigendian\n':
+            endianess = '>'
         else:
-            return np.array(value_, dtype=np.void)
+            endianess = '<'
+        return endianess
     
-    
-    def build_data_dtype(self, k_, v_):
+    def set_header_info(self):
         
-        if k_ == 'number_format' and v_ ==:
+        hdr = self
+
+        header_dict = hdr._header_dict
+
+        #Getting image dimensions
+        dim = header_dict['number_of_dimensions']
+
+        shape = []
+        for i in range(dim):
+            key_ = 'matrix_size_['+str(i+1)+']'
+            shape.append(header_dict[key_])
+        
+        hdr.set_data_shape(tuple(shape))
+        
+        #Get image voxel size
+        zooms = []
+        if 'mmppix' in header_dict.keys():
+            zooms = np.abs(np.fromstring(header_dict['mmppix'], sep=' '))
+            zooms = np.insert(zooms, 3, 1)
+        else:
+            #hdr['orient'] = self.get_flip_orientation(header_dict['orientation'])
+            for i in range(3):
+                key_ = 'scaling_factor_(mm/pixel)_['+str(i+1)+']'
+                zooms.append(header_dict[key_])
+                zooms.append(1)
+        hdr.set_zooms(tuple(zooms))
+          
+        if 'center' in header_dict.keys():
+            origin = np.fromstring(header_dict['center'], sep=' ')
+            origin = np.append(origin, [0., 0.])
+            hdr['origin'] = origin
+        
+        
+        if header_dict['imagedata_byte_order'] == 'bigendian\n':
+            endianess = '>'
+        else:
+            endianess = '<'
+        
+        if header_dict['number_format'] == 'float\n':
+            format_ = 'f'
+        else:
+            format_ = 'i'
             
-            
+        bytes_ = str(header_dict['number_of_bytes_per_pixel'])
         
+        data_type = endianess + format_ + bytes_
         
-    
+        #hdr.set_data_dtype(data_type)
+        
+        return
+
+        
+
 class WashUImage(AnalyzeImage):
     
     def __init__(self):
@@ -163,8 +217,16 @@ class WashUImage(AnalyzeImage):
         
         
 
-def load_4dfp(filename, **kwargs):
+def clean_key(key_):
+        
+        return key_.replace(' ','_').split('__')[0]
     
-    return klass
+    
+def parse_key_value(key_, value_, dtype_):
+        
+        if key_ in dtype_.fields.keys():
+            return np.array(value_, dtype=dtype_[key_])
+        else:
+            return np.array(value_, dtype=np.void)
     
     
